e73cfc1c7398a5bfde7a3bf1db668973ccf21462
dem <admin@askodia.com>
2022-05-26 12:40:13 -0500

    it looks nicer; favicon; all the stuff

--- dempred/src/App.jsx
+++ dempred/src/App.jsx
@@ -1,42 +1,32 @@
 import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
 
+
+function predict() {
+  // based on a random number, predict if the video is demonitized and then display the result to the id 'text'
+
+  Math.random() > 0.5 ? document.getElementById('text').innerHTML = 'your video is demonitized' : document.getElementById('text').innerHTML = 'your video is not demonitized'
+
+  // if video is demonitized, display css animation demonitized
+
+  if (document.getElementById('text').innerHTML === 'your video is demonitized') {
+    document.getElementById('text').style.animation = 'demonitized 1s normal forwards ease-in-out'
+  }
+  else {
+    document.getElementById('text').style.animation = 'monitized 1s normal forwards ease-in-out'
+  }
+}
+
+
+
+
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+        <button  id='text' onClick={predict}>click to see if your video is demonitized</button>
       </header>
     </div>
   )
